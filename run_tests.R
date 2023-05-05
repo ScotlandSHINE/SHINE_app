d5cf8eef164dd993f49b16b38552216abf5b73f5
Andrew Baxter <andrewbaxter439@gmail.com>
2023-05-05 18:14:40 +0100

    updated tests

--- run_tests.R
+++ run_tests.R
@@ -4,3 +4,11 @@ library(shinytest)
 test_that("Application works", {
   expect_pass(testApp("app", compareImages = TRUE))
 })
+
+
+test_that("Component files compile", {
+  expect_silent(source("app/components/influences.R"))
+  expect_silent(source("app/components/time_changes.R"))
+  expect_silent(source("app/components/compare_countries.R"))
+  expect_silent(source("app/components/vars_by_age.R"))
+})
