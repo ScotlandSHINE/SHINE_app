a7d23f67f590170a2d29ede98ca9f3cf214f627d
Andrew Baxter <andrewbaxter439@gmail.com>
2023-05-05 21:40:46 +0100

    working ubuntu tests

--- app/tests/shinytest/test_influences.R
+++
@@ -1,7 +0,0 @@
-app <- ShinyDriver$new("../../")
-app$snapshotInit("test_influences")
-
-app$setInputs(influences = "click")
-app$setInputs(`influences-exposure` = "Family helpful")
-app$setInputs(`influences-outcome` = "Life satisfaction")
-app$snapshot()
