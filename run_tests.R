f65cfa8bb52981fb000731a3bc6b2ca5197c79b2
Andrew Baxter <andrewbaxter439@gmail.com>
2023-05-05 18:22:26 +0100

    changed test order

--- run_tests.R
+++ run_tests.R
@@ -1,14 +1,14 @@
 library(testthat)
 library(shinytest)
 
-test_that("Application works", {
-  expect_pass(testApp("app", compareImages = TRUE))
-})
-
-
 test_that("Component files compile", {
   expect_silent(source("app/components/influences.R"))
   expect_silent(source("app/components/time_changes.R"))
   expect_silent(source("app/components/compare_countries.R"))
   expect_silent(source("app/components/vars_by_age.R"))
 })
+
+test_that("Application works", {
+  expect_pass(testApp("app", compareImages = FALSE))
+})
+
